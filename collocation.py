0292279a4f073f9e2b412c593cad70fafd769ed8
jaiveerbhaidasna <43055551+jaiveerbhaidasna@users.noreply.github.com>
2022-04-18 22:08:24 -0500

    made a class for Collocation

--- collocation.py
+++ collocation.py
@@ -1,27 +1,34 @@
 import multiprocessing as mp    
 
-def main():
-    dict = {}
-    word = ""
-    prev_word = ""
-    with open('test.txt', mode='r') as f:
-        contents = f.read()
+class Collocation:
 
-    for i in range(len(contents)):
-        if contents[i] == " ":
-            if word in dict.keys():
-                if prev_word in dict[word].keys():
-                    dict[word][prev_word] += 1
+    def seq_collocate(self):
+        dict = {}
+        word = ""
+        prev_word = ""
+        with open('test.txt', mode='r') as f:
+            contents = f.read()
+
+        for i in range(len(contents)):
+            if contents[i] == " ":
+                if word in dict.keys():
+                    if prev_word in dict[word].keys():
+                        dict[word][prev_word] += 1
+                    else:
+                        dict[word][prev_word] = 1
                 else:
-                    dict[word][prev_word] = 1
+                    dict[word] = {}
+                prev_word = word
+                word = ""
             else:
-                dict[word] = {}
-            prev_word = word
-            word = ""
-        else:
-            word = word + contents[i]
+                word = word + contents[i]
 
-    print(dict)
+        print(dict)
+
+
+def main():
+    c = Collocation()
+    c.seq_collocate()
 
 if __name__ == "__main__":
     main()
