41e3f3dce825ca274ec087ec9b5c14b78b6ecf70
benjwkim <47643049+benjwkim@users.noreply.github.com>
2022-04-19 20:53:30 -0500

    Update collocation.py

--- collocation.py
+++ collocation.py
@@ -1,35 +1,62 @@
 import multiprocessing as mp    
 
 class Collocation:
+    dict = {}
+    word = ""
+    prev_word = ""
 
-    def seq_collocate(self):
-        dict = {}
-        word = ""
-        prev_word = ""
+    def seq_collocate(self, num_pairs):
         with open('test.txt', mode='r') as f:
             contents = f.read()
 
         for i in range(len(contents)):
             if contents[i] == " ":
-                if word in dict.keys():
-                    if prev_word in dict[word].keys():
-                        dict[word][prev_word] += 1
+                if self.word in self.dict.keys():
+                    if self.prev_word in self.dict[self.word].keys():
+                        self.dict[self.word][self.prev_word] += 1
                     else:
-                        dict[word][prev_word] = 1
+                        self.dict[self.word][self.prev_word] = 1
                 else:
-                    dict[word] = {}
-                prev_word = word
-                word = ""
+                    self.dict[self.word] = {}
+                self.prev_word = self.word
+                self.word = ""
             else:
-                word = word + contents[i]
+                self.word = self.word + contents[i]
 
-        print(dict)
+        result = []
+        for x in range(num_pairs):
+            temp = {}
+            max = 0
+            max_key1 = ""
+            max_key2 = ""
+            for i in self.dict:
+                for j in self.dict[i]:
+                    if max < self.dict[i][j]:
+                        max = self.dict[i][j]
+                        max_key1 = i
+                        max_key2 = j
+            temp[max_key2] = max_key1
+            result.append(temp)
+            self.dict[max_key1][max_key2] = -1
+        
+        return result
+
+    def par_collocation(self, num_pairs):
+        with open('test.txt', mode='r') as f:
+            contents = f.read()
+        
+        p = mp.Pool(mp.cpu_count())
+        
+
+    def helper(self):
+
+                    
+
 
 
 def main():
     c = Collocation()
-    c.seq_collocate()
+    seq_collocation = c.seq_collocate(5)
 
 if __name__ == "__main__":
     main()
-
